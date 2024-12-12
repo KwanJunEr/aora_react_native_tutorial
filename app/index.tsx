23a3a38c1501cdda05bf33358083c3155e14d6d2
KwanJunEr <jonastheitguy@gmail.com>
2024-12-12 16:26:19 +0800

    coompleted setup and nativewind configuration

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import {StatusBar} from 'expo-status-bar'
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style = {styles.container}
+      
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+     <Text className="text-2xl text-red-50">NativeWind</Text>
+      <StatusBar style = "auto"/>
+      <Link href = "/profile" style={{color: "blue"}}>Go to Profile</Link>
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: "#fff",
+    alignItems : 'center',
+    justifyContent: 'center'
+  }
+})
