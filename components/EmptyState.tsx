69ff2c54bda5680f04b0bd2a9710fb7fdddc238b
KwanJunEr <jonastheitguy@gmail.com>
2024-12-14 23:06:24 +0800

    completed half an hour of coding

--- components/EmptyState.tsx
+++ components/EmptyState.tsx
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { router } from "expo-router";
 
-const EmptyState = () => {
+import { images } from "../constants";
+import CustomButton from "./CustomButton";
+
+const EmptyState = ({title, subtitle} : any) => {
   return (
-    <View>
-      <Text>EmptyState</Text>
+    <View className="justify-center items-center px-4">
+      <Image
+        source={images.empty}
+        className="w-[270px] h-[250px]"
+        resizeMode="contain"
+      /> 
+      <Text className="text-xl font-psemibold text-center text-white mt-2">{title}</Text>
+      <Text className="font-pmedium text-sm text-gray-100 ">{subtitle}</Text>
+        <CustomButton 
+        title= "Create video"
+        handlePress = {()=> router.push("/create")}
+        containerStyles = "w-full my-5"
+        />
     </View>
-  )
-}
+  );
+};
 
-export default EmptyState
+export default EmptyState;
