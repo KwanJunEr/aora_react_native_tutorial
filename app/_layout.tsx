239a43c99aa4e28fedea79aa08f45c0f4a5964a6
KwanJunEr <jonastheitguy@gmail.com>
2024-12-14 18:27:15 +0800

    update  _layout.tsx  in app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -28,13 +28,14 @@ export default function RootLayout() {
   return (
     <GlobalProvider>
     <Stack>
+      <Stack.Screen name = "(tabs)" options = {{headerShown: false}}/>
       <Stack.Screen name = "index" options={{headerShown: false}}/>
       <Stack.Screen name = "(auth)" options ={{
         headerShown: false
       }}/>
-      {/* <Stack.Screen name = "/search/[query]" options ={{
+       <Stack.Screen name = "/search/[query]" options ={{
         headerShown: false
-      }}/> */}
+      }}/>
     </Stack>
     </GlobalProvider>
   )
