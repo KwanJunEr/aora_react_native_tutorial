662f6d569714e3d96e7a8c4fb3a10b04f45ee6f6
KwanJunEr <jonastheitguy@gmail.com>
2024-12-23 13:24:10 +0800

    completed the trending and videocard but unsure why video would not be able to play

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -1,8 +1,9 @@
 import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import { icons } from '@/constants'
+import {Video, ResizeMode} from 'expo-av'
 
-const VideoCard = ({video : {title, thumbnail, video, creator: {username, avatar}}} :any) => {
+const VideoCard = ({video : {title, thumbnail, video, creator: {username, avatar}}}) => {
     const [play, setPlay] = useState(false);
   return (
     <View className='flex-col items-center px-4 mb-14'>
@@ -34,7 +35,18 @@ const VideoCard = ({video : {title, thumbnail, video, creator: {username, avatar
         </View>
         
         {play ? (
-            <Text className='text-white'>Playing</Text>
+            <Video
+                source={{uri: video}}
+                className='w-full h-60 rounded-xl mt-3'
+                resizeMode={ResizeMode.CONTAIN}
+                useNativeControls
+                shouldPlay
+                onPlaybackStatusUpdate={(status)=>{
+                   if(status.didJustFinish){
+                    setPlay(false);
+                   }
+                }}
+            />
         ) : 
             <TouchableOpacity
             activeOpacity={0.7}
